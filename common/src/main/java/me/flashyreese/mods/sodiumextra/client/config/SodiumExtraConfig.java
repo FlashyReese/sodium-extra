f39345a5807c66f5c747dc89bd2ef14bdb375317
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2025-12-13 17:24:34 -0600

    change: Fix import

--- common/src/main/java/me/flashyreese/mods/sodiumextra/client/config/SodiumExtraConfig.java
+++ common/src/main/java/me/flashyreese/mods/sodiumextra/client/config/SodiumExtraConfig.java
@@ -17,10 +17,7 @@ import net.minecraft.network.chat.ComponentUtils;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.level.material.FogType;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class SodiumExtraConfig implements ConfigEntryPoint {
