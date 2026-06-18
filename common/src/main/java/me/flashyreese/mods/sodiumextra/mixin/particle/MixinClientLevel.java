1adac0a66d500ef7972037edeff69aeaadb394c1
Yao Chung Hu <reeszrbteam@gmail.com>
2026-06-17 19:43:35 -0600

    change: Preserve weather effects when disabling rain splash (#523)

--- common/src/main/java/me/flashyreese/mods/sodiumextra/mixin/particle/MixinClientLevel.java
+++ common/src/main/java/me/flashyreese/mods/sodiumextra/mixin/particle/MixinClientLevel.java
@@ -4,10 +4,12 @@ import me.flashyreese.mods.sodiumextra.client.SodiumExtraClientMod;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientLevel.class)
@@ -26,10 +28,10 @@ public class MixinClientLevel {
         }
     }
 
-    @Inject(method = "tickWeatherEffects", at = @At(value = "HEAD"), cancellable = true)
-    public void tickRainSplashing(CallbackInfo ci) {
-        if (!SodiumExtraClientMod.options().particleSettings.particles || !SodiumExtraClientMod.options().particleSettings.rainSplash) {
-            ci.cancel();
+    @Redirect(method = "tickWeatherEffects", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;addParticle(Lnet/minecraft/core/particles/ParticleOptions;DDDDDD)V"))
+    public void addRainSplashParticle(ClientLevel instance, ParticleOptions particleOptions, double x, double y, double z, double xd, double yd, double zd) {
+        if (SodiumExtraClientMod.options().particleSettings.particles && SodiumExtraClientMod.options().particleSettings.rainSplash) {
+            instance.addParticle(particleOptions, x, y, z, xd, yd, zd);
         }
     }
 }
