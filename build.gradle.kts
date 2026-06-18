13f631d9428283178d253a8dc4c9bd56580a3dc6
Yao Chung Hu <reeszrbteam@gmail.com>
2026-06-17 18:57:32 -0600

    change: Bump to `0.8.9` for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ val FABRIC_API_VERSION by extra { "0.153.0+26.1.2" }
 // https://semver.org/
 val MAVEN_GROUP by extra { "me.flashyreese.mods" }
 val ARCHIVE_NAME by extra { "sodium-extra" }
-val MOD_VERSION by extra { "0.8.8" }
+val MOD_VERSION by extra { "0.8.9" }
 val SODIUM_VERSION by extra { "0.9.1-beta.2+mc26.1.2" }
 
 allprojects {
