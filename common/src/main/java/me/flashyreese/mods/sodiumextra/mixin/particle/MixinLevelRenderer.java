59ca0c8277ea29c930988966145fa12e710abf0e
Yao Chung Hu <reeszrbteam@gmail.com>
2026-06-28 11:18:45 -0600

    change: Preserve weather effects when disabling rain splash (#523)

--- common/src/main/java/me/flashyreese/mods/sodiumextra/mixin/particle/MixinLevelRenderer.java
+++ common/src/main/java/me/flashyreese/mods/sodiumextra/mixin/particle/MixinLevelRenderer.java
@@ -1,29 +1,29 @@
 package me.flashyreese.mods.sodiumextra.mixin.particle;
 
 import me.flashyreese.mods.sodiumextra.client.SodiumExtraClientMod;
-import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.WeatherEffectRenderer;
-import net.minecraft.server.level.ParticleStatus;
+import net.minecraft.core.particles.ParticleOptions;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(WeatherEffectRenderer.class)
 public class MixinLevelRenderer {
-    @Inject(method = "tickRainParticles", at = @At(value = "HEAD"), cancellable = true)
-    public void tickRainSplashing(ClientLevel clientLevel, Camera camera, int i, ParticleStatus particleStatus, int j, CallbackInfo ci) {
-        if (!(SodiumExtraClientMod.options().particleSettings.particles && SodiumExtraClientMod.options().particleSettings.rainSplash)) {
-            ci.cancel();
-        }
-    }
-
     @Inject(method = "render", at = @At(value = "HEAD"), cancellable = true, locals = LocalCapture.NO_CAPTURE)
     private void renderWeather(CallbackInfo ci) {
         if (!(SodiumExtraClientMod.options().detailSettings.rainSnow)) {
             ci.cancel();
+        }
+    }
+
+    @Redirect(method = "tickRainParticles", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;addParticle(Lnet/minecraft/core/particles/ParticleOptions;DDDDDD)V"))
+    private void addRainSplashParticle(ClientLevel level, ParticleOptions particleOptions, double x, double y, double z, double xd, double yd, double zd) {
+        if (SodiumExtraClientMod.options().particleSettings.particles && SodiumExtraClientMod.options().particleSettings.rainSplash) {
+            level.addParticle(particleOptions, x, y, z, xd, yd, zd);
         }
     }
 }
