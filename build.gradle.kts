2b5082424d2f183f219706dd9699511f93058c3b
Yao Chung Hu <reeszrbteam@gmail.com>
2026-07-09 18:18:27 -0600

    chore: bump version to 0.9.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ val PARCHMENT_VERSION by extra { null }
 // https://semver.org/
 val MAVEN_GROUP by extra { "me.flashyreese.mods" }
 val ARCHIVE_NAME by extra { "sodium-extra" }
-val MOD_VERSION by extra { "0.9.1" }
+val MOD_VERSION by extra { "0.9.2" }
 val SODIUM_VERSION by extra { "0.8.12+mc1.21.1" }
 val GREENLIGHT_VERSION by extra { "0.1.0+mc1.21.10" }
 
