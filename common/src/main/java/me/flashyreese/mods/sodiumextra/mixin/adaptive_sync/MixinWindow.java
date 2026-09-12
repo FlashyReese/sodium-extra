d1b0653b2d7ae7ad67a96a5010d5197e0827246b
Yao Chung Hu <reeszrbteam@gmail.com>
2026-09-12 13:44:01 +0800

    chore: remove unused mixin imports

--- common/src/main/java/me/flashyreese/mods/sodiumextra/mixin/adaptive_sync/MixinWindow.java
+++ common/src/main/java/me/flashyreese/mods/sodiumextra/mixin/adaptive_sync/MixinWindow.java
@@ -3,7 +3,6 @@ package me.flashyreese.mods.sodiumextra.mixin.adaptive_sync;
 import com.mojang.blaze3d.platform.Window;
 import me.flashyreese.mods.sodiumextra.client.SodiumExtraClientMod;
 import me.flashyreese.mods.sodiumextra.client.config.SodiumExtraGameOptions;
-import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
--- common/src/main/java/me/flashyreese/mods/sodiumextra/mixin/render/block/entity/MixinBeaconRenderer.java
+++ common/src/main/java/me/flashyreese/mods/sodiumextra/mixin/render/block/entity/MixinBeaconRenderer.java
@@ -6,7 +6,6 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BeaconRenderer;
 import net.minecraft.world.level.block.entity.BeaconBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Coerce;
 import org.spongepowered.asm.mixin.injection.Inject;
