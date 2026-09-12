6d0b6f570310b078b9e8ad56815f8dd3ef152fbb
Yao Chung Hu <reeszrbteam@gmail.com>
2026-09-12 13:44:08 +0800

    chore: remove unused mixin import

--- common/src/main/java/me/flashyreese/mods/sodiumextra/mixin/fog/MixinFogRenderer.java
+++ common/src/main/java/me/flashyreese/mods/sodiumextra/mixin/fog/MixinFogRenderer.java
@@ -18,7 +18,6 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
